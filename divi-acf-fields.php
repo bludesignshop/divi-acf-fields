38821294a61c12575ed25cab9e949a8e6e235347
Eduard Ungureanu <u.eduard.email@gmail.com>
2020-11-09 11:04:02 +0200

    change the name of the plugin

--- divi-acf-fields.php
+++ divi-acf-fields.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: Use Gallery and Video ACF filed inside Divi 
+ * Plugin Name: Divi ACF Gallery and oEmbed ACF fields 
  * Plugin URI: https://divi.tech
  * Description: Allows the usage of ACF Gallery and oEmebed fields type to be display inside Divi's Code Module
  * Version: 1.0.0
