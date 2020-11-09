ea8b21578382a73aac6570c4885777e450915ac4
Eduard Ungureanu <u.eduard.email@gmail.com>
2020-11-09 11:02:06 +0200

    update index.php file

--- divi-acf-fields.php
+++ divi-acf-fields.php
@@ -1,1 +1,9 @@
 <?php
+/**
+ * Plugin Name: Use Gallery and Video ACF filed inside Divi 
+ * Plugin URI: https://divi.tech
+ * Description: Allows the usage of ACF Gallery and oEmebed fields type to be display inside Divi's Code Module
+ * Version: 1.0.0
+ * Author: Eduard Ungureanu
+ * Author URI: https://divi.tech
+ */
