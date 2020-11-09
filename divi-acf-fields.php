e1c2c67fc128022d6f85882a47c6a469fe761278
Eduard Ungureanu <u.eduard.email@gmail.com>
2020-11-09 11:08:33 +0200

    Video shortcode

--- divi-acf-fields.php
+++ divi-acf-fields.php
@@ -7,3 +7,28 @@
  * Author: Eduard Ungureanu
  * Author URI: https://divi.tech
  */
+
+ /*
+ * Usage [dt-yt-acf-video acf_id='my-video']
+ * - my-video - is the ACF oEmbed field's ID;
+ */
+
+add_shortcode('dt-yt-acf-video', 'dt_show_video');
+function dt_show_video($atts = '') {
+    global $post;
+    
+    $wporg_atts = shortcode_atts([
+		'acf_id' => '',
+	], $atts);
+	   
+    $custom_fields = get_post_custom($post->ID);
+    $video_custom_field = $custom_fields[$wporg_atts['acf_id']];
+    
+    foreach ( $video_custom_field as $key => $value ) {
+    	$video_url = explode('?v=', $value);
+    	$video_id = $video_url[1];
+  	}
+  	
+    
+    return '<iframe width="560" height="315" src="https://www.youtube.com/embed/'.$video_id.'" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+}
